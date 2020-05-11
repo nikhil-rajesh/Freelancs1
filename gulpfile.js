fdf25bf0131b40b12a4241a2dbd83051ff193864
Ph4t3 <nikhilr.v2.0@gmail.com>
2020-05-12 02:10:57 +0530

    frontend finished

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp    = require("gulp");
+const sass    = require("gulp-sass");
+const uglify  = require('gulp-uglify');
+const concat  = require('gulp-concat');
+
+
+/*
+  generate the css with sass
+*/
+gulp.task('css', function() {
+  return gulp.src('./src/_assets/stylesheets/app.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    })
+    .on('error', sass.logError))
+    .pipe(gulp.dest('./src/static/css'));
+});
+
+/*
+  Watch folders for changess
+*/
+gulp.task("watch", function() {
+  gulp.watch('./src/_assets/**/*.scss', gulp.parallel('css'));
+});
+
+
+/*
+  Let's build this sucker.
+*/
+gulp.task('build', gulp.parallel(
+  'css'
+));
